4eba0ae8945445bbf1ba3c545f2fdd56d08736dd
The Aufish <the@aufish.com>
2025-08-09 19:06:33 -0400

    checkpoint

--- .common.mk
+++ .common.mk
@@ -3,19 +3,21 @@ DIST_DIR       ?= $(ROOT_DIR)/dist
 SRC_DIR        ?= $(ROOT_DIR)/src
 VAULT_DIR      ?= $(ROOT_DIR)/vault
 
-GIT_COMMIT_SHORT := $(shell git rev-parse --short HEAD 2>/dev/null || echo "unknown")
+CUR_GIT_TAG	      ?= $(shell git describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0")
+CUR_GIT_TAG_MAJOR := $(shell echo $(CUR_GIT_TAG) | sed -E 's/^v([0-9]+)\..*$$/\1/')
+CUR_GIT_TAG_MINOR := $(shell echo $(CUR_GIT_TAG) | sed -E 's/^v[0-9]+\.([0-9]+).*$$/\1/')
+CUR_GIT_TAG_PATCH := $(shell echo $(CUR_GIT_TAG) | sed -E 's/^v[0-9]+\.[0-9]+\.([0-9]+).*$$/\1/')
+GIT_REVISION      := $(shell git rev-parse --short HEAD 2>/dev/null || echo "unknown")
 
-GIT_TAG	  := $(shell git describe --tags --abbrev=0 2>/dev/null || echo "v0.0.0")
-TAG_MAJOR := $(shell echo $(GIT_TAG) | sed -E 's/^v([0-9]+)\..*$$/\1/')
-TAG_MINOR := $(shell echo $(GIT_TAG) | sed -E 's/^v[0-9]+\.([0-9]+).*$$/\1/')
-TAG_PATCH := $(shell echo $(GIT_TAG) | sed -E 's/^v[0-9]+\.[0-9]+\.([0-9]+).*$$/\1/')
+FILE_TAG       ?= $(shell cat -n $(ROOT_DIR)/VERSION 2>/dev/null || echo "0.0.0")
+FILE_TAG_MAJOR := $(shell echo $(FILE_TAG) | sed -E 's/^v([0-9]+)\..*$$/\1/')
+FILE_TAG_MINOR := $(shell echo $(FILE_TAG) | sed -E 's/^v[0-9]+\.([0-9]+).*$$/\1/')
+FILE_TAG_PATCH := $(shell echo $(FILE_TAG) | sed -E 's/^v[0-9]+\.[0-9]+\.([0-9]+).*$$/\1/')
 
-ifeq [[ -f $(ROOT_DIR)/.version ]] || (echo "Error: .common.mk not found in $(ROOT_DIR)"; exit 1)
+ifeq ([[ $(CUR_GIT_TAG_MAJOR) -lt $(FILE_TAG_MAJOR) ]], 0)
+else ifeq ([[ $(CUR_GIT_TAG_MAJOR) -lt $(FILE_TAG_MAJOR) ]], 0)
+else ifeq ([[ $(CUR_GIT_TAG_MAJOR) -lt $(FILE_TAG_MAJOR) ]], 0)
 endif
 
-TAG_MAJOR := $(shell echo $(GIT_TAG) | sed -E 's/^v([0-9]+)\..*$$/\1/')
-
-
-
-NEXT_PATCH := $(shell echo $$(($(GIT_TAG_PATCH)+1)))
- := v$(GIT_TAG_MAJOR).$(GIT_TAG_MINOR).$(NEXT_TAG_PATCH)
+NEXT_TAG_PATCH := $(shell echo $$(($(GIT_TAG_PATCH)+1)))
+NEXT_TAG := v$(GIT_TAG_MAJOR).$(GIT_TAG_MINOR).$(NEXT_TAG_PATCH)
